89672d63f57159f0ed091e72beab9de19e85f180
Oluwaseyi Ogunnowo <seyiogunnowo@yahoo.com>
2023-01-13 09:42:11 +0100

    Added .sh files for creating custom_python kernel

---
+++ on-start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# OVERVIEW
+# This script installs a custom, persistent installation of conda on the Notebook Instance's EBS volume, and ensures
+# that these custom environments are available as kernels in Jupyter.
+# 
+# The on-start script uses the custom conda environment created in the on-create script and uses the ipykernel package
+# to add that as a kernel in Jupyter.
+#
+# For another example, see:
+# https://docs.aws.amazon.com/sagemaker/latest/dg/nbi-add-external.html#nbi-isolated-environment
+
+sudo -u ec2-user -i <<'EOF'
+unset SUDO_UID
+WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda/
+source "$WORKING_DIR/miniconda/bin/activate"
+for env in $WORKING_DIR/miniconda/envs/*; do
+    BASENAME=$(basename "$env")
+    source activate "$BASENAME"
+    python -m ipykernel install --user --name "$BASENAME" --display-name "Custom ($BASENAME)"
+done
+# Optionally, uncomment these lines to disable SageMaker-provided Conda functionality.
+# echo "c.EnvironmentKernelSpecManager.use_conda_directly = False" >> /home/ec2-user/.jupyter/jupyter_notebook_config.py
+# rm /home/ec2-user/.condarc
+EOF
+
+echo "Restarting the Jupyter server.."
+# restart command is dependent on current running Amazon Linux and JupyterLab
+CURR_VERSION_AL=$(cat /etc/system-release)
+CURR_VERSION_JS=$(jupyter --version)
+
+if [[ $CURR_VERSION_JS == *$"jupyter_core     : 4.9.1"* ]] && [[ $CURR_VERSION_AL == *$" release 2018"* ]]; then
+	sudo initctl restart jupyter-server --no-wait
+else
+	sudo systemctl --no-block restart jupyter-server.service
+fi
